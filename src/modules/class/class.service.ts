4bca42712cc2bc223d8898de15f23e67680ce390
Zang <bt.hoanggiang@gmail.com>
2024-12-26 05:33:11 +0700

    feat: create class when import

--- src/modules/class/class.service.ts
+++ src/modules/class/class.service.ts
@@ -6,6 +6,8 @@ import { filterQuery } from 'src/common/utils/filterQuery';
 import { paginateByQuery } from 'src/common/utils/paginate';
 import { FindOptionsRelations, Repository } from 'typeorm';
 import { Class } from './entities/class.entity';
+import { ClassResponseDto } from './dto/Class.response.dto';
+import { ClassRequestDto } from './dto/Class.request.dto';
 
 @Injectable()
 export class ClassService extends BaseService<Class> {
@@ -45,5 +47,27 @@ export class ClassService extends BaseService<Class> {
     return this.repo.findOne({
       where: { class_id: id },
     });
+  }
+
+  async findOrCreateClassByNameAndSemesterId(classRequest: ClassRequestDto) {
+    const classEntity = await this.repo.findOne({
+      where: {
+        display_name: classRequest.displayName,
+        semester_id: classRequest.semesterId,
+        subject_id: classRequest.subjectId,
+      },
+    });
+    if (!classEntity) {
+      const newClass = this.repo.create({
+        display_name: classRequest.displayName,
+        semester_id: classRequest.semesterId,
+        subject_id: classRequest.subjectId,
+        lecturer_id: classRequest.lecturerId,
+        program: classRequest.program,
+      });
+      await this.repo.save(newClass);
+      return new ClassResponseDto(newClass);
+    }
+    return new ClassResponseDto(classEntity);
   }
 }
